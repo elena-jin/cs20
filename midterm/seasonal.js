34dfe3bb714404aacc3433d4a0003f3880b5f0e0
Elena Jin <83528210+elena-jin@users.noreply.github.com>
2025-10-28 23:04:21 -0400

    seasonal deleted img

--- midterm/seasonal.js
+++ midterm/seasonal.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('drinks-container');
 
-  // Directly use the JSON array (no fetch)
+  // use json arr
   const drinks = [
-    { name: "Savannah Spice", type: "hot", price: "$5.25", image: "images/pumpkin.png" },
-    { name: "Mask Off Matcha", type: "cold", price: "$5.00", image: "images/cider.png" },
-    { name: "Hibiscus Heat", type: "hot", price: "$5.50", image: "images/maple.png" }
+    { name: "Savannah Spice", type: "hot", price: "$5.25"},
+    { name: "Mask Off Matcha", type: "cold", price: "$5.00"},
+    { name: "Hibiscus Heat", type: "hot", price: "$5.50"}
   ];
 
-  // Render each drink card
+
+// drink card
   drinks.forEach(d => {
     const card = document.createElement('div');
     card.className = 'drink-card';
     card.innerHTML = `
-      <img src="${d.image}" alt="${d.name}" class="drink-image">
       <h3>${d.name}</h3>
       <p>${d.price}</p>
       <p class="type">${d.type === 'hot' ? '🔥 Hot' : '🧊 Cold'}</p>
